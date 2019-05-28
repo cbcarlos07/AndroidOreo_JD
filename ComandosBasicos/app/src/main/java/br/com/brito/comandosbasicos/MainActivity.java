3e28b1e79b73e9e0874f162825f7e467e66427c5
Carlos Bruno <cbcarlos08@gmail.com>
2019-05-28 11:57:42 -0400

    80. Checkbox

--- ComandosBasicos/app/src/main/java/br/com/brito/comandosbasicos/MainActivity.java
+++ ComandosBasicos/app/src/main/java/br/com/brito/comandosbasicos/MainActivity.java
@@ -5,11 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private TextInputEditText campoProduto;
     private TextView resultado;
+    private CheckBox cbBranco, cbVerde, cbVermelho;
+    private List<String> check = new ArrayList<String>() ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +23,29 @@ public class MainActivity extends AppCompatActivity {
 
         campoProduto = findViewById( R.id.etNomeProduto );
         resultado = findViewById( R.id.tvResultado );
+        cbBranco  = findViewById( R.id.cbBranco );
+        cbVerde  = findViewById( R.id.cbVerde );
+        cbVermelho  = findViewById( R.id.cbVermelho );
     }
+    public void verificaCheck(){
+        check.clear();
+        if( cbBranco.isChecked() ){
+            check.add( cbBranco.getText().toString() );
+        }
 
+        if( cbVerde.isChecked() ){
+            check.add( cbVerde.getText().toString() );
+        }
+
+        if( cbVermelho.isChecked() ){
+            check.add( cbVermelho.getText().toString() );
+        }
+
+        resultado.setText( check.toString() );
+    }
     public void btEnviar(View view){
-        String nomeProduto = campoProduto.getText().toString() + ".";
-        resultado.setText( nomeProduto );
-
+        /*String nomeProduto = campoProduto.getText().toString() + ".";
+        resultado.setText( nomeProduto );*/
+        verificaCheck();
     }
 }
